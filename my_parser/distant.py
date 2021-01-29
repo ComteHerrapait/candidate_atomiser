180eb598a0303ba1678303d4c20706666c56cfee
Emilion Goddet <e.goddet58@gmail.com>
2021-01-29 16:15:59 +0100

    export raw data to aws and import processed data
    all import and export functions are complete
    tried with a fake 'result.csv' to check if import processed_data was ok
    
    Co-Authored-By: ViviLeVivif <56339386+ViviLeVivif@users.noreply.github.com>

--- my_parser/distant.py
+++ my_parser/distant.py
@@ -4,17 +4,17 @@ import scp
 import os
 
 def import_data(conf_path="../config/connect.conf"):
-    # To change paths, check hdp.conf
+    # To change paths, check connect.conf
     config = configparser.ConfigParser()
     config.read(conf_path)
     
     user = config['HDP']['USER']
     password = config['HDP']['PASSWORD']
-    ip = config['HDP']['IP_HDP']
+    ip = config['HDP']['HOST_NAME_HDP']
     port = config['HDP']['PORT_HDP']
     hdp_path = config['HDP']['PATH_HDFS']
     vm_path = config['HDP']['PATH_VM']
-    local_path = config['HDP']['PATH_LOCAL']
+    local_path = config['DEFAULT']['PATH_LOCAL_RAW']
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -39,23 +39,56 @@ def import_data(conf_path="../config/connect.conf"):
 
 
 def export_data(conf_path="config/connect.conf"):
-    # To change paths, check hdp.conf
+    # To change paths, check connect.conf
     config = configparser.ConfigParser()
     config.read(conf_path)
     
-    user = config['AWS']['USER_NAME']
-    hostname = config['AWS']['HOST_NAME']
+    user = config['AWS']['USER']
+    hostname = config['AWS']['HOST_NAME_AWS']
     key_file = config['AWS']['KEY_FILE']
+    local_path = config['DEFAULT']['PATH_LOCAL_RAW']
+    aws_raw_path = config['AWS']['PATH_AWS_RAW']
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname, username=user, pkey=key_file)
-    stdin, stdout, stderr = ssh.exec_command('ls')
+    ssh.connect(hostname, username=user, key_filename=key_file)
+
+    with scp.SCPClient(ssh.get_transport()) as scp1:
+        stdin, stdout, stderr = ssh.exec_command('mkdir -p -m=00755 ' + aws_raw_path)
+
+        scp1.put(local_path + 'data.json', aws_raw_path)
+        scp1.put(local_path + 'categories_string.csv', aws_raw_path)
+        scp1.put(local_path + 'label.csv', aws_raw_path)
+        print("Data is now on the VM")
+
+def import_processed_data(conf_path="config/connect.conf"):
+    # To change paths, check connect.conf
+    config = configparser.ConfigParser()
+    config.read(conf_path)
+    
+    user = config['AWS']['USER']
+    hostname = config['AWS']['HOST_NAME_AWS']
+    key_file = config['AWS']['KEY_FILE']
+    local_path = config['DEFAULT']['PATH_LOCAL_PROCESSED']
+    aws_processed_path = config['AWS']['PATH_AWS_PROCESSED']
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=user, key_filename=key_file)
+
+    with scp.SCPClient(ssh.get_transport()) as scp1:
+        if not os.path.exists(local_path):
+            os.makedirs(local_path)
+            print("Local directory created")
+        
+        scp1.get(aws_processed_path + 'result.csv', local_path)
+        print("Processed data is now on your machine")
 
 
 if __name__ == "__main__" : 
     print("running distant parser")
     #import_data()
-    export_data()
+    #export_data()
+    import_processed_data()
 else :
     print("imported ", __name__)
